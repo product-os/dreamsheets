7166793d6301e05d3e8e1294334f859ad5748d78
AlidaOdendaal <alida@balena.io>
2022-01-07 11:45:59 +0200

    Experimental version of new writeToSheet function
    Change-type: patch
    Signed-off-by: AlidaOdendaal <alida@balena.io>

--- lib/spreadsheet.ts
+++ lib/spreadsheet.ts
@@ -3,91 +3,6 @@ type RangeTuple =
 	| [number, number, number]
 	| [number, number, number, number];
 
-export function clearSheet(
-	sheet: GoogleAppsScript.Spreadsheet.Sheet,
-	startingRow: number = 1,
-	startingColumn: number = 1,
-	rowsLength: number = sheet.getMaxRows(),
-	colsLenghth: number = sheet.getMaxColumns(),
-) {
-	sheet
-		.getRange(startingRow, startingColumn, rowsLength, colsLenghth)
-		.clearContent();
-}
-
-/* Runs the given function with parameters and writes the results to the outputTab
- * .. srow and scol are the starting cell for the output
- * .. errorTab lists the errors raised when running the function
- */
-export function updateSheet(
-	func: (...args: any[]) => any[][],
-	params: any[],
-	outputSheetName: string,
-	startingRow: number,
-	startingColumn: number,
-	errorTabName: string,
-): void {
-	const ss = SpreadsheetApp.getActiveSpreadsheet();
-	const logSheet = ss.getSheetByName(errorTabName);
-
-	if (!logSheet) {
-		throw new Error(`Error logging sheet ${errorTabName} is not found`);
-	}
-
-	console.log(`Now running ${func.name}`);
-	let res: any[][] | undefined;
-	try {
-		res = func(...params);
-	} catch (err) {
-		const now = new Date();
-		logSheet.appendRow([
-			now,
-			func.name,
-			(err as Error).message,
-			(err as Error).stack,
-		]);
-	}
-	if (res != null) {
-		writeToSheet(
-			outputSheetName,
-			startingRow,
-			startingColumn,
-			res,
-			errorTabName,
-		);
-	} else {
-		throw new Error(`Error running ${func.name}`);
-	}
-
-	console.log(`Done running ${func.name}`);
-}
-
-export function writeToSheet(
-	outputSheetName: string,
-	startingRow: number,
-	startingColumn: number,
-	writeArray: any[][],
-	errorTabName: string,
-) {
-	const ss = SpreadsheetApp.getActiveSpreadsheet();
-	const logSheet = ss.getSheetByName(errorTabName);
-	const outputSheet = ss.getSheetByName(outputSheetName);
-
-	if (!logSheet) {
-		throw new Error(`Error logging sheet ${errorTabName} is not found`);
-	}
-	if (!outputSheet) {
-		throw new Error(`writeTo sheet ${outputSheetName} is not found`);
-	}
-
-	const rows = writeArray.length;
-	const cols = writeArray[0].length;
-	console.log('Writing the results. # of rows: ' + rows + ' and cols: ' + cols);
-	clearSheet(outputSheet, startingRow, startingColumn, rows, cols);
-	outputSheet
-		.getRange(startingRow, startingColumn, rows, cols)
-		.setValues(writeArray);
-}
 
 export function readSheet(
 	sheetName: string,
@@ -132,4 +47,80 @@ export function readSheet(
 		`Successfully read contents of sheet "${sheetName}" in spreadsheet "${ssName}".`,
 	);
 	return rangeValues;
+}
+
+
+export function writeToSheet(
+	sheetName: string,
+	writeArray: any[][],
+	{
+		startRow,
+		startColumn,
+		templateSheetName,
+		ss,
+		eraseAllDataInSheet,
+		onlyWriteIfEmpty
+	}: {
+		startRow?: number,
+		startColumn?: number,
+		templateSheetName?: string,
+		ss?: GoogleAppsScript.Spreadsheet.Spreadsheet,
+		eraseAllDataInSheet?: boolean,		
+		onlyWriteIfEmpty?: boolean
+	} = {}
+	) {
+	// If no spreadsheet provided, read the currently active spreadsheet:
+	if (!ss) {
+		ss = SpreadsheetApp.getActiveSpreadsheet();
+	}
+	const ssName = ss.getName();
+	let sheet = ss.getSheetByName(sheetName);
+
+	// Check whether template was provided:
+	let templateSheet
+	if (templateSheetName) {
+		templateSheet = ss.getSheetByName(templateSheetName)
+		if (!templateSheet) {
+			throw new Error(`A template sheet named ${templateSheetName} does not exist in spreadsheet "${ssName}"!`)
+		}		
+	}
+
+	// Check for existence of sheet in spreadsheet:
+	if (!sheet) {			
+		sheet = ss.insertSheet(sheetName, {template: templateSheet})
+		console.log(`Sheet ${sheetName} created in spreadsheet ${ssName}.`)
+	}
+
+	// Check whether starting row and column were provided:
+	if (!startRow) {
+		startRow = 1
+	}
+	if (!startColumn) {
+		startColumn = 1
+	}
+
+	// Output dimensions:
+	const numRows = writeArray.length
+	const numColumns = writeArray[0].length
+
+	// Get relevant range:
+	let range
+	if (!eraseAllDataInSheet) { // Parameter either not provided, or set to false.
+		range = sheet.getRange(startRow, startColumn, numRows, numColumns) // Assuming writeArray is rectangular.
+	} else {
+		range = sheet.getDataRange()
+	}
+	
+	// Check whether write should only take place if range is empty:
+	if (onlyWriteIfEmpty === true) {
+		if (!range.isBlank()) {
+			throw new Error(`Operation would overwrite existing data! Cancelling.`)
+		}
+	}
+	
+	// Clear appropriate range on sheet:
+	range.clearContent()
+
+	// Write output:
+	sheet.getRange(startRow, startColumn, numRows, numColumns).setValues(writeArray)
 }
